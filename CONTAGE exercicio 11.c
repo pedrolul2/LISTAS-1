9668de91f822da4f12a4f755513064cd3580b82c
Pedro Lucas Alves Viana <89168920+pedrolul2@users.noreply.github.com>
2021-09-17 17:19:17 -0300

    Add files via upload

---
+++ CONTAGE exercicio 11.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int contagem;
+    while(contagem<=100){
+        printf("%d\n",contagem);
+        ++contagem;
+    }
+}
---
+++ questão 12.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int contagem=100;
+    while(contagem>=1){
+        printf("%d\n",contagem);
+        --contagem;
+    }
+}
